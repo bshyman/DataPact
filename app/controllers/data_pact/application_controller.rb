b485974e0d1467efdf3bb7e0729d917b70df1bc6
Benjamin Shyman <bshyman@gmail.com>
2023-07-18 22:37:05 -0500

    routes page wip

--- app/controllers/data_pact/application_controller.rb
+++ app/controllers/data_pact/application_controller.rb
@@ -1,6 +1,12 @@
 module DataPact
   class ApplicationController < ActionController::Base
     def index
+      @routes = Rails.application.routes.routes.map do |route|
+        {
+          path: route.path.spec.to_s,
+          controller_action: [route.defaults[:controller], '#', route.defaults[:action]].join,
+        }
+      end
       render 'application/index'
     end
   end
